03809f087c93f9d2c8ffd28cccad81206d3b86ac
shanphd <klgowrishankarphd@gmail.com>
2024-02-17 15:32:56 +0530

    new book

---
+++ 5.Github_packages/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # List your dependencies here
+    ],
+)
